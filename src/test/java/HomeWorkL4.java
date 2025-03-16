0b69b613d5aaf07d76799f1a9a9dac19fba89fc2
Oleg <namozov@dev.vtb.ru>
2025-03-16 12:55:12 +0300

    added fix after review

--- src/test/java/HomeWorkL4.java
+++ src/test/java/HomeWorkL4.java
@@ -1,7 +1,10 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.DragAndDropOptions;
 import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
@@ -10,12 +13,13 @@ import static com.codeborne.selenide.Selenide.*;
 public class HomeWorkL4 {
     @BeforeAll
     static void configurationBrowser() {
+        Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://github.com";
-        com.codeborne.selenide.Configuration.pageLoadStrategy = "eager";
+        Configuration.pageLoadStrategy = "eager";
     }
 
     @AfterEach
-    void closeChrome() {
+    void closeWebDrivers() {
         closeWebDriver();
     }
 
@@ -32,7 +36,6 @@ public class HomeWorkL4 {
     @Test
     @DisplayName("Проверка dragAndDrop")
     public void dragAndDropTest() {
-
         SelenideElement columnA = $x("//div[@id='column-a']");
         SelenideElement columnB = $x("//div[@id='column-b']");
         open("https://the-internet.herokuapp.com/drag_and_drop");
